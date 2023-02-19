d09d4fa0fb461d3de6624c9cdcd4ff54892f7e71
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-02-19 11:24:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,42 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-let userAnswer = prompt("кількість хвилин");
+// let userAnswer = prompt("кількість хвилин");
 
-let hours = Math.floor(userAnswer / 60);
-hours = String(hours).padStart(2, "0");
+// let hours = Math.floor(userAnswer / 60);
+// hours = String(hours).padStart(2, "0");
 
-let minutes = userAnswer % 60;
-minutes = String(minutes).padStart(2, "0");
+// let minutes = userAnswer % 60;
+// minutes = String(minutes).padStart(2, "0");
 
-console.log(`You entered ${hours}:${minutes}`);
+// console.log(`You entered ${hours}:${minutes}`);
+
+//Напишіть цикл, який виводить в консоль
+//числа от max до min по убыванию
+//Виведіть в консоль суму усіх парних чисел
+
+// const min = 0;
+// const max = 10;
+// let sum = 0;
+// for (let i = max; i >= min; i -= 1){
+//     console.log(i);
+//     if (i % 2 === 0) {
+//     sum += i;    
+//     }
+
+// }
+// console.log(sum);
+    
+function summary(min, max) {
+    let sum = 0;
+    for (let i = max; i >= min; i -= 1){
+        if (i % 2 !== 0) {
+            continue;
+        }
+        sum += i;
+        console.log(i);
+    }
+    return sum;
+}
+console.log(summary(10, 16));
+
