6ac2db43eb1718c8367431b5411c451690f077d0
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-03-05 11:22:29 +0200

    task-2 completed

--- lesson2.js
+++ lesson2.js
@@ -5,19 +5,46 @@
 //3 замінить значення premium на false
 //4 виводить зміст об'єкта users у форматі
 //ключ:значення використовуя Object.keys() та for...of
-const user = {
-    name: "John",
-    age: 20,
-    hobby: "tenis",
-    premium: true,
-  };
+// const user = {
+//     name: "John",
+//     age: 20,
+//     hobby: "tenis",
+//     premium: true,
+//   };
 
-  user.mood = 'happy';
-  user.hobby = 'skydiving';
-  user.premium = false;
+//   user.mood = 'happy';
+//   user.hobby = 'skydiving';
+//   user.premium = false;
 
-  const userKeys = Object.keys(user);
+//   const userKeys = Object.keys(user);
 
-  for (const key of userKeys)  {
-    console.log(`${key}: ${user[key]}`)
-  }
+//   for (const key of userKeys)  {
+//     console.log(`${key}: ${user[key]}`)
+//   }
+
+// ================
+
+//2. У нас є об'єкт, в якому зберігаються зарплати
+//нашої команди
+//Напишіть код для додавання усіх зарплат та
+//збережіть його результат в змінній sum.
+//Якщо об'єкт salaries пустий, то результат має бути 0
+
+const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
+};
+
+const salary = Object.values(salaries);
+
+console.log(salary.reduce((sum, item) => (sum + item), 0));
+
+// let sum = 0;
+
+// for (const salary of Object.values(salaries)) {
+//   sum += salary;
+// };
+
+// console.log(sum);
+
