0a7ae7ffae2a2259acfe00e618976d539ac352a9
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-04-02 11:27:24 +0300

    task 2

--- lesson4.js
+++ lesson4.js
@@ -4,38 +4,78 @@
 // Парним лі вказати жовтий фон, непарним синій
 // Використовуй createElement
 
-const firstList = document.createElement("ol");
-const addBtn = document.createElement("button");
-const removeBtn = document.createElement("button");
-addBtn.textContent = "ADD";
-removeBtn.textContent = "REMOVE";
-const inputList = document.createElement("input");
+// const firstList = document.createElement("ol");
+// const addBtn = document.createElement("button");
+// const removeBtn = document.createElement("button");
+// addBtn.textContent = "ADD";
+// removeBtn.textContent = "REMOVE";
+// const inputList = document.createElement("input");
 
-document.body.append(firstList, addBtn, removeBtn, inputList);
+// document.body.append(firstList, addBtn, removeBtn, inputList);
 
-addBtn.addEventListener("click", onAddClick);
-function onAddClick() {
-  // console.log(inputList.value);
-  if (inputList.value.trim() === "") {
-    return;
-  }
-  const newLi = document.createElement("li");
-  newLi.textContent = inputList.value;
-  firstList.append(newLi);
-  //   if (firstList.children.length % 2 === 0) {
-  //     newLi.style.background = "yellow";
-  //   } else {
-  //     newLi.style.background = "blue";
-  //   }
-  newLi.style.background =
-    firstList.children.length % 2 === 0 ? "yellow" : "blue";
-  inputList.value = "";
+// addBtn.addEventListener("click", onAddClick);
+// function onAddClick() {
+//   // console.log(inputList.value);
+//   if (inputList.value.trim() === "") {
+//     return;
+//   }
+//   const newLi = document.createElement("li");
+//   newLi.textContent = inputList.value;
+//   firstList.append(newLi);
+//   //   if (firstList.children.length % 2 === 0) {
+//   //     newLi.style.background = "yellow";
+//   //   } else {
+//   //     newLi.style.background = "blue";
+//   //   }
+//   newLi.style.background =
+//     firstList.children.length % 2 === 0 ? "yellow" : "blue";
+//   inputList.value = "";
+// }
+
+// removeBtn.addEventListener("click", onRemoveClick);
+
+// function onRemoveClick() {
+//   if (firstList.lastChild) {
+//     firstList.lastChild.remove();
+//   }
+// }
+
+// ==== TASK 2 ====
+
+// Створити невелику гру
+// Спочатку на екрані з'являється якась фігура рандомного коліру в рандомному місті
+// Натискаючі на фігуру, вона змінює свою форму, колір, місце розташування
+
+const forms = [
+  "width: 100px; height: 100px; border-width: 1px;",
+  "width: 100px; height: 100px; border-radius: 50%;",
+  "width: 150px; height: 100px; border-width: 1px;",
+  "width: 200px; height: 100px; border-radius: 100px / 50px; ",
+  "width: 150px; height: 100px; transform: skew(20deg)",
+];
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-removeBtn.addEventListener("click", onRemoveClick);
-
-function onRemoveClick() {
-  if (firstList.lastChild) {
-    firstList.lastChild.remove();
-  }
+function randomither(max) {
+  return Math.floor(Math.random() * max);
 }
+
+function chaneNpcStyles(npcEl) {
+  npcEl.style.cssText = forms[randomither(forms.length)];
+  npcEl.style.backgroundColor = getRandomHexColor();
+  npcEl.style.position = "absolute";
+  npcEl.style.top = randomither(100) + "%";
+  npcEl.style.left = randomither(100) + "%";
+}
+
+function createFormMarkup(forms) {
+  const npcEl = document.createElement("div");
+  chaneNpcStyles(npcEl);
+  document.body.append(npcEl);
+
+  npcEl.addEventListener("click", () => chaneNpcStyles(npcEl));
+}
+
+createFormMarkup(forms);
